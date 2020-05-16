c44f70478ca4eddc2e76437208dd9ce907d4e050
gentelyang <852221609@qq.com>
2020-05-16 21:45:29 +0800

    Update serving.sh

--- serving.sh
+++ serving.sh
@@ -124,7 +124,7 @@ function build_cpu_server(){
 
 function build_app() {
   setproxy
-  pip3 install paddle_serving_client opencv-python Pillow
+  pip3 install paddlepaddle paddlehub ujson paddle_serving_client opencv-python Pillow
   yum install -y libXext libSM libXrender
   cd ${build_path}
   if [ -d build ];then
@@ -347,7 +347,7 @@ function end_hook(){
 
 function main() {
   before_hook
-  build_client
+  build_all_whl
   end_hook
 }
 
