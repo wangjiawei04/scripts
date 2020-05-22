2eb316b9614341c210151fd7228a263b3112ac5a
gentelyang <852221609@qq.com>
2020-05-22 15:55:51 +0800

    Update run_iPipe.sh

--- run_iPipe.sh
+++ run_iPipe.sh
@@ -333,6 +333,8 @@ function fit_a_line_http() {
   run_cpu_env
   cd ${build_path}/python/examples/fit_a_line
   python3 -m paddle_serving_server.serve --model uci_housing_model --thread 10 --port 8871 --name uci > http_log 2>&1 &
+  sleep 10
+  cat http_log
   curl -H "Content-Type:application/json" -X POST -d '{"feed":[{"x": [0.0137, -0.1136, 0.2553, -0.0692, 0.0582, -0.0727, -0.1583, -0.0584, 0.6283, 0.4919, 0.1856, 0.0795, -0.0332]}], "fetch":["price"]}' http://${host}:8871/uci/prediction
 }
 
