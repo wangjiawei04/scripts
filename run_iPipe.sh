dfb60cd1c9b68c8668323a9a35b470b8390b49d2
gentelyang <852221609@qq.com>
2020-05-25 11:35:12 +0800

    Update run_iPipe.sh

--- run_iPipe.sh
+++ run_iPipe.sh
@@ -12,7 +12,7 @@ echo "################################################################"
 
 build_path=/workspace/Serving/Serving
 build_whl_list=(build_gpu_server build_client build_cpu_server build_app)
-rpc_model_list=(bert_rpc_gpu bert_rpc_cpu faster_rcnn_model_rpc ResNet50_rpc ResNet101_rpc lac_rpc cnn_rpc bow_rpc lstm_rpc fit_a_line_rpc)
+rpc_model_list=(bert_rpc_gpu bert_rpc_cpu faster_rcnn_model_rpc ResNet50_rpc ResNet101_rpc lac_rpc criteo_ctr_rpc criteo_ctr_gpu_rpc cnn_rpc bow_rpc lstm_rpc fit_a_line_rpc)
 http_model_list=(fit_a_line_http lac_http cnn_http bow_http lstm_http ResNet50_http bert_http)
 
 
@@ -196,7 +196,8 @@ function criteo_ctr_rpc(){
   setproxy
   run_cpu_env
   cd ${build_path}/python/examples/criteo_ctr
-  sh get_data.sh >/dev/null 2>&1
+  wget --no-check-certificate https://sys-p0.bj.bcebos.com/models/ctr_data.tar.gz >/dev/null 2>&1
+  tar -zxf ctr_data.tar.gz
   wget https://paddle-serving.bj.bcebos.com/criteo_ctr_example/criteo_ctr_demo_model.tar.gz >/dev/null 2>&1
   tar xf criteo_ctr_demo_model.tar.gz >/dev/null 2>&1
   mv models/ctr_client_conf .
