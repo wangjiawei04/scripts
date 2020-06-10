00c295eec4d8b5594b8639c1b4528fafb3787481
gentelyang <852221609@qq.com>
2020-06-10 20:11:13 +0800

    Update iPipe_py2.sh

--- iPipe_py2.sh
+++ iPipe_py2.sh
@@ -12,7 +12,8 @@ echo "################################################################"
 
 build_path=/workspace/Serving
 build_whl_list=(build_gpu_server build_client build_cpu_server build_app)
-rpc_model_list=(bert_rpc_gpu bert_rpc_cpu faster_rcnn_model_rpc ResNet50_rpc lac_rpc cnn_rpc bow_rpc lstm_rpc fit_a_line_rpc cascade_rcnn_rpc deeplabv3_rpc mobilenet_rpc unet_rpc resnetv2)
+rpc_model_list=(bert_rpc_gpu bert_rpc_cpu faster_rcnn_model_rpc ResNet50_rpc lac_rpc cnn_rpc bow_rpc lstm_rpc \
+fit_a_line_rpc cascade_rcnn_rpc deeplabv3_rpc mobilenet_rpc unet_rpc resnetv2)
 http_model_list=(fit_a_line_http lac_http cnn_http bow_http lstm_http ResNet50_http bert_http)
 
 
