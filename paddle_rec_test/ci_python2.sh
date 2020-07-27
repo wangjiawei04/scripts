35a05a4819a58ce9104ff746de3ef47423d332c1
gentelyang <852221609@qq.com>
2020-07-27 20:05:43 +0800

    Update ci_python2.sh

--- paddle_rec_test/ci_python2.sh
+++ paddle_rec_test/ci_python2.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
+function setproxy(){
+  export http_proxy=${proxy}
+  export https_proxy=${proxy}
+}
+
 function prepare(){
-#    git clone http://github.com//PaddleRec.git
-  #  pushd PaddleRec
+    setproxy
     cd PaddleRec
     pip uninstall paddle-rec -y
     python setup.py install
     pip uninstall paddlepaddle -y
     wget https://paddle-wheel.bj.bcebos.com/0.0.0-gpu-cuda10-cudnn7-mkl/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl
     pip install paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl
-   # pip install paddlepaddle-gpu==1.7.2.post107
     pip install nose
     pip install ruamel.yaml
     cd ../ && mkdir test_logs
