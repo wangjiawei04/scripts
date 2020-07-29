ebc6c2657c68c3a360485066f0633ca085d4f1d5
gentelyang <852221609@qq.com>
2020-07-29 14:43:00 +0800

    Update run_ipipe_py3.sh

--- run_ipipe_py3.sh
+++ run_ipipe_py3.sh
@@ -12,9 +12,9 @@ echo "################################################################"
 
 build_path=/workspace/Serving/
 build_whl_list=(build_gpu_server build_client build_cpu_server build_app)
-rpc_model_list=(bert_rpc_gpu bert_rpc_cpu criteo_ctr_with_cube_rpc faster_rcnn_model_rpc ResNet50_rpc lac_rpc \
+rpc_model_list=(bert_rpc_gpu bert_rpc_cpu faster_rcnn_model_rpc ResNet50_rpc lac_rpc \
 cnn_rpc bow_rpc lstm_rpc fit_a_line_rpc cascade_rcnn_rpc deeplabv3_rpc mobilenet_rpc unet_rpc resnetv2_rpc \
-ocr_rpc criteo_ctr_rpc_cpu criteo_ctr_rpc_gpu yolov4_rpc_gpu)
+ocr_rpc)
 http_model_list=(fit_a_line_http lac_http cnn_http bow_http lstm_http ResNet50_http bert_http)
 
 
@@ -597,10 +597,11 @@ function run_http_models(){
 
 function end_hook(){
   cd ${build_path}
+  kill_server_process
   echo "===========files==========="
   ls -hlst
   echo "=========== end ==========="
-  kill_server_process
+  
 }
 
 function main() {
