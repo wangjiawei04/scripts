5e8a4258ad40783cb24905f8b0b59739ee1212c3
gentelyang <852221609@qq.com>
2020-07-29 15:25:11 +0800

    Update iPipe_py2.sh

--- iPipe_py2.sh
+++ iPipe_py2.sh
@@ -12,7 +12,7 @@ echo "################################################################"
 
 build_path=/workspace/Serving
 build_whl_list=(build_gpu_server build_client build_cpu_server build_app)
-rpc_model_list=(bert_rpc_gpu bert_rpc_cpu criteo_ctr_with_cube_rpc faster_rcnn_model_rpc ResNet50_rpc lac_rpc \
+rpc_model_list=(bert_rpc_gpu bert_rpc_cpu faster_rcnn_model_rpc ResNet50_rpc lac_rpc \
 cnn_rpc bow_rpc lstm_rpc fit_a_line_rpc cascade_rcnn_rpc deeplabv3_rpc mobilenet_rpc unet_rpc resnetv2_rpc \
 ocr_rpc criteo_ctr_rpc_cpu yolov4_rpc_gpu)
 http_model_list=(fit_a_line_http lac_http cnn_http bow_http lstm_http ResNet50_http bert_http)
@@ -595,10 +595,10 @@ function run_http_models(){
 }
 function end_hook(){
   cd ${build_path}
+  kill_server_process
   echo "===========files==========="
   ls -hlst
   echo "=========== end ==========="
-  kill_server_process
 }
 
 function main() {
