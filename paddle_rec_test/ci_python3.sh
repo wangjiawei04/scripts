f1b74183049d2e03e1b9db68b90c16a0282455d8
gentelyang <852221609@qq.com>
2020-08-03 10:53:17 +0800

    Update ci_python3.sh

--- paddle_rec_test/ci_python3.sh
+++ paddle_rec_test/ci_python3.sh
@@ -13,7 +13,7 @@ function prepare(){
     python3 -m pip uninstall paddlepaddle -y
 #     wget https://paddle-wheel.bj.bcebos.com/0.0.0-gpu-cuda10-cudnn7-mkl/paddlepaddle_gpu-0.0.0-cp35-cp35m-linux_x86_64.whl
 #     python3 -m pip install paddlepaddle_gpu-0.0.0-cp35-cp35m-linux_x86_64.whl
-    python3 -m pip install paddlepaddle-gpu==1.8.2
+    python3 -m pip install paddlepaddle-gpu==1.8.2.post107
     python3 -m pip install nose
     python3 -m pip install ruamel.yaml
     cd ../ && mkdir test_logs
