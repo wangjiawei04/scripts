33a0c27024f11159be3bf51f4dc80529bfcf2b20
gentelyang <852221609@qq.com>
2020-08-04 14:49:38 +0800

    Update ci_python3.sh

--- paddle_rec_test/ci_python3.sh
+++ paddle_rec_test/ci_python3.sh
@@ -11,8 +11,11 @@ function prepare(){
     python3 -m pip uninstall paddle-rec -y
     python3 setup.py install
     python3 -m pip uninstall paddlepaddle -y
-    wget http://yq01-gpu-255-125-19-00.epc.baidu.com:8988/1.7.2/paddlepaddle_gpu-1.7.2.post107-cp35-cp35m-linux_x86_64.whl
-    python3 -m pip install paddlepaddle_gpu-1.7.2.post107-cp35-cp35m-linux_x86_64.whl
+    wget https://paddle-wheel.bj.bcebos.com/1.8.3-gpu-cuda10-cudnn7-mkl%2Fpaddlepaddle_gpu-1.8.3.post107-cp35-cp35m-linux_x86_64.whl
+    mv 1.8.3-gpu-cuda10-cudnn7-mkl%2Fpaddlepaddle_gpu-1.8.3.post107-cp35-cp35m-linux_x86_64.whl paddlepaddle_gpu-1.8.3.post107-cp35-cp35m-linux_x86_64.whl
+    python3 -m pip install paddlepaddle_gpu-1.8.3.post107-cp35-cp35m-linux_x86_64.whl
+#    wget http://yq01-gpu-255-125-19-00.epc.baidu.com:8988/1.7.2/paddlepaddle_gpu-1.7.2.post107-cp35-cp35m-linux_x86_64.whl
+#    python3 -m pip install paddlepaddle_gpu-1.7.2.post107-cp35-cp35m-linux_x86_64.whl
 #     wget https://paddle-wheel.bj.bcebos.com/0.0.0-gpu-cuda10-cudnn7-mkl/paddlepaddle_gpu-0.0.0-cp35-cp35m-linux_x86_64.whl
 #     python3 -m pip install paddlepaddle_gpu-0.0.0-cp35-cp35m-linux_x86_64.whl
    # python3 -m pip install paddlepaddle-gpu==1.8.2.post107
