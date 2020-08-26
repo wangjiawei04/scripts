49b649cb0b7074d928c16ad7396300f9372896ce
gentelyang <liyang109@baidu.com>
2020-08-26 15:16:00 +0800

    Update ci_python2.sh

--- paddle_rec_test/ci_python2.sh
+++ paddle_rec_test/ci_python2.sh
@@ -11,7 +11,7 @@ function prepare(){
     pip uninstall paddle-rec -y
     python setup.py install
     pip uninstall paddlepaddle -y
-    wget https://paddle-wheel.bj.bcebos.com/1.8.3-gpu-cuda10-cudnn7-mkl%2Fpaddlepaddle_gpu-1.8.3.post107-cp27-cp27mu-linux_x86_64.whl
+    wget https://paddle-wheel.bj.bcebos.com/1.8.3-gpu-cuda10-cudnn7-mkl%2Fpaddlepaddle_gpu-1.8.3.post107-cp27-cp27mu-linux_x86_64.whl --no-check-certificate
     mv 1.8.3-gpu-cuda10-cudnn7-mkl%2Fpaddlepaddle_gpu-1.8.3.post107-cp27-cp27mu-linux_x86_64.whl paddlepaddle_gpu-1.8.3.post107-cp27-cp27mu-linux_x86_64.whl
     pip install paddlepaddle_gpu-1.8.3.post107-cp27-cp27mu-linux_x86_64.whl
 #    wget http://yq01-gpu-255-125-19-00.epc.baidu.com:8988/1.7.2/paddlepaddle_gpu-1.7.2.post107-cp27-cp27mu-linux_x86_64.whl
