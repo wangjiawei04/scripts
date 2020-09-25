662160eec7974ad4dca1aca6b04dc6851d39288b
gentelyang <liyang109@baidu.com>
2020-09-25 14:47:18 +0800

    Update run_fleetx_py3.sh

--- run_fleetx_py3.sh
+++ run_fleetx_py3.sh
@@ -90,8 +90,9 @@ function ctr_app() {
     cd ${fleetx_path}/examples
     sed -i "s/epoch=10/epoch=1/g" ctr_app.py
     sed -i "s/ctr_data/train_data/raw_data/g" ctr_app.py
-    cp -r /root/.cache/dist_data/serving/criteo_ctr_with_cube/raw_data/part-0 ./raw_data
-    cp -r /root/.cache/dist_data/serving/criteo_ctr_with_cube/raw_data/part-1 ./raw_data
+    mkdir raw_data
+    cp -r /root/.cache/dist_data/serving/criteo_ctr_with_cube/raw_data/part-0 ./raw_data/
+    cp -r /root/.cache/dist_data/serving/criteo_ctr_with_cube/raw_data/part-1 ./raw_data/
     fleetrun ctr_app.py
     check_result $FUNCNAME
     kill_fleetx_process
