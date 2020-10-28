e11b6d56c66d9057be08fcf1196ee55a6b092f6e
gentelyang <liyang109@baidu.com>
2020-10-28 16:58:06 +0800

    Update ci_python3.sh

--- paddle_rec_test/ci_python3.sh
+++ paddle_rec_test/ci_python3.sh
@@ -8,14 +8,14 @@ function setproxy(){
 function prepare(){
     #setproxy
     cd PaddleRec
-    python3 -m pip uninstall paddle-rec -y
+    python -m pip uninstall paddle-rec -y
 #     pip3 install skbuild 
 #     pip3 install opencv-python==4.2.0.32 
-    python3 setup.py install
-    python3 -m pip uninstall paddlepaddle -y
+    python setup.py install
+    python -m pip uninstall paddlepaddle -y
     wget https://paddle-wheel.bj.bcebos.com/1.8.5-gpu-cuda10-cudnn7-mkl%2Fpaddlepaddle_gpu-1.8.5.post107-cp35-cp35m-linux_x86_64.whl --no-check-certificate
     mv 1.8.5-gpu-cuda10-cudnn7-mkl%2Fpaddlepaddle_gpu-1.8.5.post107-cp35-cp35m-linux_x86_64.whl paddlepaddle_gpu-1.8.5.post107-cp35-cp35m-linux_x86_64.whl
-    python3 -m pip install paddlepaddle_gpu-1.8.5.post107-cp35-cp35m-linux_x86_64.whl
+    python -m pip install paddlepaddle_gpu-1.8.5.post107-cp35-cp35m-linux_x86_64.whl
 #     wget https://paddle-wheel.bj.bcebos.com/1.8.3-gpu-cuda10-cudnn7-mkl%2Fpaddlepaddle_gpu-1.8.3.post107-cp35-cp35m-linux_x86_64.whl
 #     mv 1.8.3-gpu-cuda10-cudnn7-mkl%2Fpaddlepaddle_gpu-1.8.3.post107-cp35-cp35m-linux_x86_64.whl paddlepaddle_gpu-1.8.3.post107-cp35-cp35m-linux_x86_64.whl
 #     python3 -m pip install paddlepaddle_gpu-1.8.3.post107-cp35-cp35m-linux_x86_64.whl
@@ -24,8 +24,8 @@ function prepare(){
 #     wget https://paddle-wheel.bj.bcebos.com/0.0.0-gpu-cuda10-cudnn7-mkl/paddlepaddle_gpu-0.0.0-cp35-cp35m-linux_x86_64.whl
 #     python3 -m pip install paddlepaddle_gpu-0.0.0-cp35-cp35m-linux_x86_64.whl
    # python3 -m pip install paddlepaddle-gpu==1.8.2.post107
-    python3 -m pip install nose
-    python3 -m pip install ruamel.yaml
+    python -m pip install nose
+    python -m pip install ruamel.yaml
     cd ../ && mkdir test_logs
     #   unset http_proxy, https_proxy
 }
