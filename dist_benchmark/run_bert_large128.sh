40460672032d6e8bc94dd03a2eefe5ae9a8de5eb
gentelyang <liyang109@baidu.com>
2020-11-02 11:46:12 +0800

    Create run_bert_large128.sh

---
+++ dist_benchmark/run_bert_large128.sh
@@ -0,0 +1,61 @@
+function run_bert_large128() {
+    echo "bert large begin run"
+    bert_large=${benchmark_path}/FleetX/2.0benchmark/collective/bert_large_max128
+    echo 'run bert_large_fp16_n1c1'
+    cd ${bert_large}/n1c1_fp16
+    rm -rf bert_benchmark.py
+    cp ../bert_benchmark_fp16.py bert_benchmark.py
+    fleetsub -f bert_large.yaml
+
+    echo 'run bert_large_fp32_n1c1'
+    cd ${bert_large}/n1c1_fp32
+    rm -rf bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
+    fleetsub -f bert_large.yaml
+
+    echo 'run bert_large_fp16_n1c8'
+    cd ${bert_large}/n1c8_fp16
+    rm -rf bert_benchmark.py
+    cp ../bert_benchmark_fp16.py bert_benchmark.py
+    fleetsub -f bert_large.yaml
+
+    echo 'run bert_large_fp32_n1c8'
+    cd ${bert_large}/n1c8_fp32
+    rm -rf bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
+    fleetsub -f bert_large.yaml
+
+    echo 'run bert_large_fp16_n2c16'
+    cd ${bert_large}/n2c16_fp16
+    rm -rf bert_benchmark.py
+    cp ../bert_benchmark_fp16.py bert_benchmark.py
+    fleetsub -f bert_large.yaml
+
+    echo 'run bert_large_fp32_n2c16'
+    cd ${bert_large}/n2c16_fp32
+    rm -rf bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
+    fleetsub -f bert_large.yaml
+
+    #30 min after
+    sleep 1200
+
+    echo 'run bert_large_fp16_n4c32'
+    cd ${bert_large}/n4c32_fp16
+    rm -rf bert_benchmark.py
+    cp ../bert_benchmark_fp16.py bert_benchmark.py
+    fleetsub -f bert_large.yaml
+
+    #20 min after
+    sleep 1200
+
+    echo 'run bert_large_fp32_n4c32'
+    cd ${bert_large}/n4c32_fp32
+    rm -rf bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
+    fleetsub -f bert_large.yaml
+
+    echo "bert large jobs submit over"
+}
+
+run_bert_large128
