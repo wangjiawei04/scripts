d033062c1d929ab97b58414d55dcc606106482ab
gentelyang <liyang109@baidu.com>
2020-11-08 19:20:10 +0800

    Update run_bert_base.sh

--- dist_benchmark/run_bert_base.sh
+++ dist_benchmark/run_bert_base.sh
@@ -10,7 +10,7 @@ function run_bert_base() {
     echo 'run bert_base_fp32_n1c1'
     cd ${bert_base}/n1c1_fp32
     rm -rf bert_benchmark.py
-    cp ../bert_base_fp32.py bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
     fleetsub -f bert_base.yaml
 
     echo 'run bert_base_fp16_n1c8'
@@ -22,7 +22,7 @@ function run_bert_base() {
     echo 'run bert_base_fp32_n1c8'
     cd ${bert_base}/n1c8_fp32
     rm -rf bert_benchmark.py
-    cp ../bert_base_fp32.py bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
     fleetsub -f bert_base.yaml
 
     echo 'run bert_base_fp16_n2c16'
@@ -34,7 +34,7 @@ function run_bert_base() {
     echo 'run bert_base_fp32_n2c16'
     cd ${bert_base}/n2c16_fp32
     rm -rf bert_benchmark.py
-    cp ../bert_base_fp32.py bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
     fleetsub -f bert_base.yaml
 
     #30 min after
@@ -52,7 +52,7 @@ function run_bert_base() {
     echo 'run bert_base_fp32_n4c32'
     cd ${bert_base}/n4c32_fp32
     rm -rf bert_benchmark.py
-    cp ../bert_base_fp32.py bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
     fleetsub -f bert_base.yaml
 
     echo "bert base jobs submit over"
