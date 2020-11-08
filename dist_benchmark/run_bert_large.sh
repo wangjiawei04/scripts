48dfcaa36a1e2d62737d19f0b9e21f002ac07499
gentelyang <liyang109@baidu.com>
2020-11-08 19:21:43 +0800

    Update run_bert_large.sh

--- dist_benchmark/run_bert_large.sh
+++ dist_benchmark/run_bert_large.sh
@@ -10,7 +10,7 @@ function run_bert_large() {
     echo 'run bert_large_fp32_n1c1'
     cd ${bert_large}/n1c1_fp32
     rm -rf bert_benchmark.py
-    cp ../bert_large_fp32.py bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
     fleetsub -f bert_large.yaml
 
     echo 'run bert_large_fp16_n1c8'
@@ -22,7 +22,7 @@ function run_bert_large() {
     echo 'run bert_large_fp32_n1c8'
     cd ${bert_large}/n1c8_fp32
     rm -rf bert_benchmark.py
-    cp ../bert_large_fp32.py bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
     fleetsub -f bert_large.yaml
 
     echo 'run bert_large_fp16_n2c16'
@@ -34,7 +34,7 @@ function run_bert_large() {
     echo 'run bert_large_fp32_n2c16'
     cd ${bert_large}/n2c16_fp32
     rm -rf bert_benchmark.py
-    cp ../bert_large_fp32.py bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
     fleetsub -f bert_large.yaml
 
     #30 min after
@@ -52,7 +52,7 @@ function run_bert_large() {
     echo 'run bert_large_fp32_n4c32'
     cd ${bert_large}/n4c32_fp32
     rm -rf bert_benchmark.py
-    cp ../bert_large_fp32.py bert_benchmark.py
+    cp ../bert_benchmark_fp32.py bert_benchmark.py
     fleetsub -f bert_large.yaml
 
     echo "bert large jobs submit over"
