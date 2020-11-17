83e25c82fb35bf498ca82e3dcdb12fce1b0076d9
gentelyang <liyang109@baidu.com>
2020-11-17 14:10:23 +0800

    Update test_dist_fleet_dygraph.py

--- dist_cts/dist_fleet_2.0/test_dist_fleet_dygraph.py
+++ dist_cts/dist_fleet_2.0/test_dist_fleet_dygraph.py
@@ -10,9 +10,9 @@
 /***************************************************************************
   *
   * Copyright (c) 2019 Baidu.com, Inc. All Rights Reserved
-  * @file test_dist_fleet_dygraph.py.py
+  * @file test_dist_fleet_dygraph.py
   * @author liyang109@baidu.com
-  * @date 2020-11-16 14:33
+  * @date 2020-11-16 14:40
   * @brief 
   *
   **************************************************************************/
@@ -30,24 +30,28 @@ class TestDygraph():
         self.single_data = []
 
     def test_dist_fleet_dygraph_api_2gpus(self):
+      """test_dist_fleet_dygraph_api_2gpus."""
       cmd='fleetrun --gpu=0,1 dist_fleet_dygraph_api.py'
       pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       pro.wait()
       pro.returncode == 0
 
     def test_dist_fleet_dygraph_api_1gpus(self):
+      """test_dist_fleet_dygraph_api_1gpus"""
       cmd='fleetrun --gpu=0 dist_fleet_dygraph_api.py'
       pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       pro.wait()
       pro.returncode == 0
 
     def test_dist_fleet_dygraph_lr_2gpus(self):
+      """test_dist_fleet_dygraph_lr_2gpus"""
       cmd='fleetrun --gpu=0,1 dist_fleet_dygraph_lr.py'
       pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       pro.wait()
       pro.returncode == 0
 
     def test_dist_fleet_dygraph_lr_1gpus(self):
+      """test_dist_fleet_dygraph_lr_2gpus"""
       cmd='fleetrun --gpu=0 dist_fleet_dygraph_lr.py'
       pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       pro.wait()
