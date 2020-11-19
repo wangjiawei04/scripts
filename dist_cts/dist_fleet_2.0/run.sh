4d0b457702662433e589a05c38e5e909e6c7d7f8
gentelyang <liyang109@baidu.com>
2020-11-19 10:22:05 +0800

    Update run.sh

--- dist_cts/dist_fleet_2.0/run.sh
+++ dist_cts/dist_fleet_2.0/run.sh
@@ -23,7 +23,8 @@ function run(){
            test_dist_fleet_static_strategy.py \
            test_dist_fleet_static_launch.py \
            test_dist_fleet_static_fleetrun.py \
-           test_dist_fleet_dygraph_gloo_init.py "
+           test_dist_fleet_dygraph_gloo_init.py \
+           test_dist_fleet_static_ctr.py "
     for file in ${cases}
     do
         echo ${file}
