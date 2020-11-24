3e9d57b88255b947c81b167f4b8daef002d6f2b4
gentelyang <liyang109@baidu.com>
2020-11-24 09:54:40 +0800

    Update run.sh

--- dist_cts/dist_fleet_2.0/run.sh
+++ dist_cts/dist_fleet_2.0/run.sh
@@ -4,7 +4,7 @@ function prepare(){
     env
     ln -s ../dist_fleet/thirdparty ./
     ln -s /ssd3/ly/cts_ce/dataset/data/ ./
-    export LD_LIBRARY_PATH=/opt/_internal/cpython-2.7.15-ucs4/lib/:/ssd2/work/418.39/lib64/:${LD_LIBRARY_PATH}
+    export LD_LIBRARY_PATH=/opt/_internal/cpython-2.7.15-ucs4/lib/:/home/work/418.39/lib64/:${LD_LIBRARY_PATH}
     export PATH=/opt/_internal/cpython-2.7.15-ucs4/bin/:${PATH}
     pip uninstall paddlepaddle-gpu -y
     pip uninstall paddlepaddle -y
