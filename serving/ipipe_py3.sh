4878c056b6e96d3033407e0cfea9fd0c8e275d49
gentelyang <liyang109@baidu.com>
2020-12-16 13:23:59 +0800

    Update ipipe_py3.sh

--- serving/ipipe_py3.sh
+++ serving/ipipe_py3.sh
@@ -15,7 +15,7 @@ export CUDA_INCLUDE_DIRS=/usr/local/cuda-10.0/include
 export PYTHONROOT=/usr
 
 build_path=/workspace/Serving/
-build_whl_list=(build_gpu_server build_client build_cpu_server build_app)
+build_whl_list=(grpc_impl build_gpu_server build_client build_cpu_server build_app)
 rpc_model_list=(bert_rpc_gpu bert_rpc_cpu faster_rcnn_model_rpc ResNet50_rpc lac_rpc \
 cnn_rpc bow_rpc lstm_rpc fit_a_line_rpc cascade_rcnn_rpc deeplabv3_rpc mobilenet_rpc unet_rpc resnetv2_rpc \
 criteo_ctr_rpc_cpu criteo_ctr_rpc_gpu ocr_rpc yolov4_rpc_gpu)
