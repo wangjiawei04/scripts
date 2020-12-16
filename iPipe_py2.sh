b6ec1a58ff428c59f8bcaa86bfd6a478325ff25c
wangjiawei04 <wangjiawei04@baidu.com>
2020-12-16 15:13:45 +0800

    fix fit a line

--- iPipe_py2.sh
+++ iPipe_py2.sh
@@ -337,7 +337,7 @@ function fit_a_line_rpc(){
   cd ${build_path}/python/examples/fit_a_line
   sh get_data.sh >/dev/null 2>&1
   sed -i 's/9393/8869/g' test_client.py
-  python3 -m paddle_serving_server.serve --model uci_housing_model --port 8869> line_rpc 2>&1 &
+  python -m paddle_serving_server.serve --model uci_housing_model --port 8869> line_rpc 2>&1 &
   sleep 5
   python test_client.py uci_housing_client/serving_client_conf.prototxt
   cat line_rpc
@@ -519,7 +519,7 @@ function fit_a_line_http() {
   unsetproxy
   run_cpu_env
   cd ${build_path}/python/examples/fit_a_line
-  python -m paddle_serving_server.serve --model uci_housing_model --thread 10 --port 8871 --name uci > http_log2 2>&1 &
+  python test_server.py &
   sleep 10
   curl -H "Content-Type:application/json" -X POST -d '{"feed":[{"x": [0.0137, -0.1136, 0.2553, -0.0692, 0.0582, -0.0727, -0.1583, -0.0584, 0.6283, 0.4919, 0.1856, 0.0795, -0.0332]}], "fetch":["price"]}' http://${host}:8871/uci/prediction
   check_result $FUNCNAME
